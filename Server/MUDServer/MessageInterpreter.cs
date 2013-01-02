4efbf060fe249440c1d55cda4dffbe6ec017150d
Jan Herlyn <amenophisthegod@googlemail.com>
2013-01-02 16:24:09 +0100

    User.cs umbenannt zu MessageInterpreter.cs

---
+++ Server/MUDServer/MessageInterpreter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MUDServer
+{
+    class MessageInterpreter
+    {
+        public delegate void writemethod(string message);
+        writemethod write;
+        uint status = 0;
+        uint substatus = 0; // Niemals vergessen, diesen status wieder auf 0 zu setzen!
+        long user_id = 42;
+
+        public MessageInterpreter(writemethod w)
+        {
+            write = w;
+            write("Wenn du ein neuer Nutzer bist tippe 'neu' ein. \nWenn du schon mal hier warst, tippe 'login' ein.");
+        }
+        public void interpretMessage(String message)
+        {
+            switch (status)
+            {
+                case 0:
+                    interpretStart(message);
+                    break;
+                case 1:
+                    interpretLogin(message);
+                    break;
+                case 2:
+                    interpretRegister(message);
+                    break;
+                case 3:
+                    interpretGameLoop(message);
+                    break;
+                default:
+                    status = 0;
+                    break;
+            }
+        }
+        private void interpretStart(string Message)
+        {
+
+        }
+        private void interpretLogin(string Message)
+        {
+        }
+        private void interpretRegister(string Message)
+        {
+        }
+        private void interpretGameLoop(string Message)
+        {
+        }
+        
+    }
+}
