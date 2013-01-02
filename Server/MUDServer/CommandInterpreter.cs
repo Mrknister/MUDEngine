be884c5cada612336e339b96ac77776d165708ff
Jan Herlyn <amenophisthegod@googlemail.com>
2013-01-02 23:26:18 +0100

    Added two new classes

---
+++ Server/MUDServer/CommandInterpreter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MUDServer
+{
+    class CommandInterpreter
+    {
+        public CommandInterpreter(long user_id)
+        {
+        }
+    }
+}
