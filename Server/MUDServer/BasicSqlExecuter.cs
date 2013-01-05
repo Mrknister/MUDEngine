8e722ec2c44320a0e273ab81cdf8712be643b22e
Jan Herlyn <amenophisthegod@googlemail.com>
2013-01-05 23:46:13 +0100

    now the sqlexecuters are derived from abstract class BasicSQLExecuter

--- Server/MUDServer/BasicSqlExecuter.cs
+++ Server/MUDServer/BasicSqlExecuter.cs
@@ -2,10 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.Data.Odbc;
+
 
 namespace MUDServer
 {
-    class BasicSqlExecuter
+    public abstract class BasicSQLExecuter
     {
+        public bool error = false;
+        public string error_string = "";
+        public string query = "";
+
+        protected List<OdbcParameter> _parameters = new List<OdbcParameter>();
+
+
+
+        public void add_parameter(long value)
+        {
+
+            OdbcParameter param = new OdbcParameter();
+            param.DbType = DbType.Int64;
+            param.Value = value;
+            _parameters.Add(param);
+        }
+        public void add_parameter(string value)
+        {
+
+            OdbcParameter param = new OdbcParameter();
+            param.DbType = DbType.String;
+            param.Value = value;
+
+            _parameters.Add(param);
+        }
+        public void add_parameter(OdbcParameter param)
+        {
+            _parameters.Add(param);
+        }
+
+        protected virtual OdbcConnection get_connection()
+        {
+            return new OdbcConnection("DRIVER={MySQL ODBC 5.2w Driver}; SERVER=mrknister.justdied.com; DATABASE=MUDEngine; UID=mudengineer;PWD=hAWFYe2YsNHXZrtF;");
+        }
+        public abstract bool execute_query();
+
     }
 }
--- Server/MUDServer/UnreadableSQLExecuter.cs
+++ Server/MUDServer/UnreadableSQLExecuter.cs
@@ -8,45 +8,18 @@ using System.Data.Odbc;
 
 namespace MUDServer
 {
-    class UnreadableSQLExecuter
+    class UnreadableSQLExecuter : BasicSQLExecuter
     {
-        public bool error = false;
-        public string error_string = "";
-        public string query;
         public int rows_affected = 0;
         
-        List<OdbcParameter> _parameters = new List<OdbcParameter>();
-
 
         public UnreadableSQLExecuter()
         {
         }
         
         
-        public void add_parameter(long value)
-        {
-            
-            OdbcParameter param = new OdbcParameter();
-            param.DbType = DbType.Int64;
-            param.Value = value;
-            _parameters.Add(param);
-        }
-        public void add_parameter(string value)
-        {
-            
-            OdbcParameter param = new OdbcParameter();
-            param.DbType = DbType.String;
-            param.Value = value;
-
-            _parameters.Add(param);
-        }
-        public void add_parameter(OdbcParameter param)
-        {
-            _parameters.Add(param);
-        }
         
-        
-        public bool execute_query()
+        public override bool execute_query()
         {
             using (OdbcConnection DbConnection = new OdbcConnection("DRIVER={MySQL ODBC 5.2w Driver}; SERVER=mrknister.justdied.com; DATABASE=MUDEngine; UID=mudengineer;PWD=hAWFYe2YsNHXZrtF;"))
             {
