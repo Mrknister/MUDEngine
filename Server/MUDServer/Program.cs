b494e96524a776261bdc7fa1151baacb0744a61c
Fuck you Git <plant410-test@yahoo.de>
2013-01-05 04:11:17 +0100

    Modified ConfigFile Extraction

--- Server/MUDServer/Program.cs
+++ Server/MUDServer/Program.cs
@@ -12,17 +12,17 @@ namespace MUDServer
         
         static void Main(string[] args)
         {
-            Config test = new Config();
+         /*   Config test = new Config();
             test.ReadFile(@"..\..\..\..\doc\configtest.txt");
             Console.WriteLine(test.DatabaseHost + " " +test.DatabasePassword +" "+ test.DatabaseUser);
             Console.ReadLine();
-
-
-
-
-         /*   Server s = new Server();
-            s.startListen();
             */
+
+
+
+           Server s = new Server();
+            s.startListen();
+            
 
         }
     }
