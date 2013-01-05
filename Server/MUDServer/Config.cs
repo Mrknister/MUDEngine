d2968bb0816141294cfa1c680f4ed4b3d8b35a9d
Fuck you Git <plant410-test@yahoo.de>
2013-01-05 02:08:57 +0100

    Modified ConfigFile Extraction

--- Server/MUDServer/Config.cs
+++ Server/MUDServer/Config.cs
@@ -2,10 +2,69 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace MUDServer
 {
-    class Class1
+    class Config
     {
+        public string DataTransfer, FileLine, DatabaseHost, DatabaseUser, DatabasePassword;
+        
+
+        public string ReadFile(String ConfigFile)
+        {
+            StreamReader ConfigFiles = new StreamReader(@ConfigFile);
+            while ((FileLine = ConfigFiles.ReadLine()) != "#")
+            {
+
+                if( FileLine.StartsWith("databaseuser =")==true)
+                {
+                    DataTransfer = SetDatabaseUser(FileLine);
+                }
+                if( FileLine.StartsWith("databasepassword =")==true)
+                {
+                    DataTransfer = SetDatabasePassword(FileLine);
+                }
+                if( FileLine.StartsWith("databasehost =")==true)
+                {
+                   DataTransfer=SetDatabaseHost(FileLine);
+                }
+                else
+                {
+                    DataTransfer = "Invalid or End of Configfile";
+                }
+
+                return DataTransfer;
+             }
+            
+           
+
+
+        }
+
+        private string SetDatabaseUser(string line)
+        {
+            
+
+            return DataTransfer;
+
+        }
+
+        private string SetDatabasePassword(string line)
+        {
+            
+
+            return DataTransfer;
+        }
+
+        private string SetDatabaseHost(string line)
+        {
+            
+
+            return DataTransfer;
+        }
+       
+      
+
     }
 }
