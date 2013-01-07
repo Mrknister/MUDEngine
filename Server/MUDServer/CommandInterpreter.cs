bb3bd23dd7954b52765185be6235b2b02bf2c0b2
Jan Herlyn <amenophisthegod@googlemail.com>
2013-01-07 01:33:54 +0100

    fucking smaller than\!

--- Server/MUDServer/CommandInterpreter.cs
+++ Server/MUDServer/CommandInterpreter.cs
@@ -17,7 +17,7 @@ namespace MUDServer
             {
                 inventar(command);
             }
-            else if (command.StartsWith("a")) 
+            else if (command.StartsWith("a ")) 
                 angriff(command);
             else if (command == "n")
             {
