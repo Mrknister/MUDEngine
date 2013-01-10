d65bb17cc45fa1191d08d171d70cc5cea6c9e3fe
JulianPLM <JulianPascal.Martin@gmail.com>
2013-01-10 01:06:20 +0100

    take ready but not working

--- Server/MUDServer/CommandInterpreter.cs
+++ Server/MUDServer/CommandInterpreter.cs
@@ -57,7 +57,7 @@ namespace MUDServer
             List<string> items = _user_data.loadItems();
             if (items.Count == 0)
             {
-                write("Du besitzt momentan nichts");
+                write("Du besitzt momentan nichts\n");
             }
             foreach (string item in items)
             {
@@ -75,14 +75,14 @@ namespace MUDServer
             long monster_lost = _user_data.attackMonster(command);
             if (_user_data.Health <= 0)
             {
-                write("Du bist tod!");
+                write("Du bist tod!\n");
             }
             if (monster_lost == -1)
             {
-                write("Ich kann das Monster "+command+" nicht finden.");
+                write("Ich kann das Monster " + command + " nicht finden.\n");
             }
             else if (monster_lost == 0)
-                write("Du hast das Monster bezwungen.");
+                write("Du hast das Monster bezwungen.\n");
             else
                 write("Das Monster hat noch " + Convert.ToString(monster_lost) + " Lebenspunkte\n");
         }
@@ -160,31 +160,31 @@ namespace MUDServer
             command = command.Trim();
             if (_enviroment_data.takeFrom("", command))
             {
-                write("Der Gegenstand wurde aufgenommen.");
+                write("Der Gegenstand wurde aufgenommen.\n");
             }
             else
             {
-                write("Der Gegenstand konnte nicht aufgenommen werden.");
+                write("Der Gegenstand konnte nicht aufgenommen werden.\n");
             }
         }
         private void prepareTakeFrom(string command)
         {
             command = command.Remove(0, 8);
             command = command.Trim();
-            write("Wähle nun den Gegenstand");
+            write("Wähle nun den Gegenstand\n");
 
 
         }
         private void executeTakeFrom(string command)
         {
             string command2 = command.Trim();
-            if (_enviroment_data.takeFrom("", command))
+            if (_enviroment_data.takeFrom(command2, command))
             {
-                write("Der Gegenstand wurde aufgenommen.");
+                write("Der Gegenstand wurde aufgenommen.\n");
             }
             else
             {
-                write("Der Gegenstand konnte nicht aufgenommen werden.");
+                write("Der Gegenstand konnte nicht aufgenommen werden.\n");
             }
         }
     }
